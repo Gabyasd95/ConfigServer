2805b27b493925018a032cb730e57f4eb213fdc4
Gabyasd95 <120499650+Gabyasd95@users.noreply.github.com>
2022-12-27 23:58:05 -0500

    Add files via upload

---
+++ ConfigServerGaby/src/test/java/com/example/configservergaby/ConfigServerGabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configservergaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerGabyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
